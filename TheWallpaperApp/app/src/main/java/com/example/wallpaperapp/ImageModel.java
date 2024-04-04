ca1963ff76d51aaad5cfc393e8b26c75ff5957c7
Arafat-hossain-Rifat <riyel.rifat321@gmail.com>
2024-04-04 16:22:37 +0600

    The-Wallpaper-App-Java

---
+++ TheWallpaperApp/app/src/main/java/com/example/wallpaperapp/ImageModel.java
@@ -0,0 +1,18 @@
+package com.example.wallpaperapp;
+
+public class ImageModel {
+    private urlModel src;
+
+    public urlModel getSrc() {
+        return src;
+    }
+
+    public void setSrc(urlModel src) {
+        this.src = src;
+    }
+
+    public ImageModel(urlModel src) {
+        this.src = src;
+    }
+
+}
